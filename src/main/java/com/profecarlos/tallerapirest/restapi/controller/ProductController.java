673ce3d4ed1cace8a8f5c9c5c762518806368eb9
Tavotsu <gustavosantanafue@gmail.com>
2025-04-25 14:11:27 -0400

    Creacion de modelo

---
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.profecarlos.tallerapirest.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.profecarlos.tallerapirest.restapi.model.Product;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProductController {
+    
+    @GetMapping("/products")
+    public String GetProduct(){
+        return "Se listan todos los productos";
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable long id){
+        return new Product(id,"Mouse",2.000);
+    }
+
+
+}
---
+++ src/main/java/com/profecarlos/tallerapirest/restapi/model/Product.java
@@ -0,0 +1,37 @@
+package com.profecarlos.tallerapirest.restapi.model;
+
+public class Product {
+    //Variables
+    private Long id;
+    private String nombre;
+    private double precio;
+
+    //Constructor
+    public Product(Long id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    //Getters & Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}

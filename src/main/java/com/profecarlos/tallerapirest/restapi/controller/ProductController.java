3d25a15528ef8e2cb1132d4c724e3da798262028
beibanezv <be.ibanezv@duocuc.cl>
2025-04-29 13:38:51 -0400

    Avances taller 2 fullstack

---
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.profecarlos.tallerapirest.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    
+    @GetMapping("/api/productos")
+    public String listarProductos(){
+        return "Se están listando todos los productos de la tienda";
+    }
+
+
+    @GetMapping("/api/productos/{id}")
+    public String producto(@PathVariable Long id){
+        return "Se esta mostrando el producto con id: " + id;
+    }
+
+    @GetMapping("/api/productos/filtrar")
+    public String productoFiltrar(
+        @RequestParam(required = false) String categoria,
+        @RequestParam(defaultValue = "0") int precioMin,
+        @RequestParam(defaultValue = "10000") int precioMax){
+        return "Se está mostrando producto con filtros: " + categoria + ", " + precioMin + " y " + precioMax;
+    }
+
+    @GetMapping("/api/productos/buscar")
+    public String producto(
+        @RequestParam(required = true) String termino,
+        @RequestParam(defaultValue = "nombre") String ordenarPor,
+        @RequestParam(defaultValue = "asc") String orden,
+        @RequestParam(defaultValue = "10") int limite){
+        return "Se están mostrando productos del tipo: " + termino + " ordenado por " + ordenarPor + " de manera " + orden + " y límite " + limite;
+    }
+
+    
+}

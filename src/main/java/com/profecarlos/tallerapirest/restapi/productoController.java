077d8d47e42e35daa28e7616e42f910207f00af6
sebastianValle <s.valle@duocuc.cl>
2025-04-30 09:36:52 -0400

    creacion de rama propia y desarrollo ejercio 1

---
+++ src/main/java/com/profecarlos/tallerapirest/restapi/productoController.java
@@ -0,0 +1,9 @@
+package com.profecarlos.tallerapirest.restapi;
+
+public class productoController {
+
+    public String listaDeProductos() {
+        return "Listando todos los productos de la tienda";
+    }
+
+}

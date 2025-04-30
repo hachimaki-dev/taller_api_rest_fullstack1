7a7bd39bc5283ccf9e9e02ad74f34ecb0841559b
laIvSxR <ma.gallardor@duocuc.cl>
2025-04-30 09:33:33 -0400

    hoy avance los ejercicios utilizando Get avance el ejercio de la 1 a las 3

--- src/main/java/com/profecarlos/tallerapirest/restapi/ProductoController/ProductoController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/ProductoController/ProductoController.java
@@ -1,16 +1,47 @@
+
 package com.profecarlos.tallerapirest.restapi.ProductoController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
-@GetMapping
-public String listaProductos() {
-return "lista de productos";
+
+    
+    @GetMapping
+    public String listarProductos() {
+        return "Se estan listando todos los productos";
+    }
+
+  
+    @GetMapping("/{id}")
+    public String obtenerProductoPorId(@PathVariable Long id) {
+        return "Se esta mostrando el producto con ID: " + id;
+    }
+
+
+@GetMapping("/filtrar")
+    public String filtrarProductos(
+            @RequestParam(required = false) String categoria,
+            @RequestParam(defaultValue = "0") Double precioMin,
+            @RequestParam(defaultValue = "1000") Double precioMax) {
+        
+        return "Filtrando productos por categoria: " + categoria + ", precio minimo: " + precioMin + ", precio maximo: " + precioMax;
+    }
+     
 }
-}
+
+
+
+  
+  
+
+
+
+

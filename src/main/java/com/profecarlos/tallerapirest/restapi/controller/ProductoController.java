7172bee62eb9975c9bbd375c606039ab2a3f01bd
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-04-30 09:31:14 -0400

    avance ejercicios

--- src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductoController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductoController.java
@@ -2,6 +2,7 @@ package com.profecarlos.tallerapirest.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.profecarlos.tallerapirest.restapi.models.Producto;
@@ -17,12 +18,19 @@ public class ProductoController {
 
     @GetMapping("/api/productos/{id}")
     public Producto getProduct(@PathVariable Long id) {
-        return new Producto(id,"producto",30000);
+        return new Producto(id,"producto",5000);
     }
     
-    @GetMapping("api/productos/filtrar")
-    public String filtarProducto(String categoria,float precioMin, float precioMax){
-        return "Categoria: " + categoria + "Precio Mínimo" + precioMin + "Precio Máximo" + precioMax;
+    @GetMapping("/api/productos/filtrar")
+    public String filtarProducto(@RequestParam(required=false) String categoria,
+                                 @RequestParam (defaultValue="0") Double precioMin,
+                                 @RequestParam (defaultValue="10000") Double precioMax){
+        return "Usted eligio: " + categoria + ", El precio máximo es de: " + precioMax + " y el precio minimo es de: " + precioMin;
+    }
+
+    @GetMapping("/api/productos/buscar")
+    public String buscarProducto (String termino){
+        return "";
     }
     
 }

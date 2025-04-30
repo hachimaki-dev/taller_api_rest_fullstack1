59b60a4c87e7c987ce35f7432f450f173e032294
Axelito777 <ax.subiabre@duocuc.cl>
2025-04-30 09:31:34 -0400

    actualizacion codigo

--- src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.profecarlos.tallerapirest.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +18,28 @@ public class ProductController {
         return "listando el productos" + id;
     }
 
+    @GetMapping("/api/productos/filtrar")
+    public String filtrar(
+        @RequestParam(required=false) String categoria,
+        @RequestParam(defaultValue="0") double valorMin,
+        @RequestParam(defaultValue="10000") double valorMax){
+            return "Filtrando productos: " +
+            (categoria != null ? " de la categoría: " + categoria : "") +
+            ", con precios entre$" + valorMin + "y $" + valorMax;
+        }
     
+    @GetMapping("/api/productos/buscar")
+    public String buscar(
+        @RequestParam(required=false) String termino,
+        @RequestParam(defaultValue="nombre") String ordenarPor,
+        @RequestParam(defaultValue="asc") String order,
+        @RequestParam(defaultValue="10") int max){
+            return "Busqueda de producto por termino: \"" + termino + "\"" +
+            ", ordenados por " + ordenarPor +
+            "("+ order +")" +
+            ", mostrando hasta " + max + " resultados.";
+
+        }
 
 
 

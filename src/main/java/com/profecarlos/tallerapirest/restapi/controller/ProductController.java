fe6f9fd6ea9c0146edb16baa6d86e5d039de34a2
beibanezv <be.ibanezv@duocuc.cl>
2025-05-02 12:55:52 -0400

    avances

--- src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.profecarlos.tallerapirest.restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.profecarlos.tallerapirest.restapi.model.Product;
 
 @RestController
 public class ProductController {
@@ -36,5 +40,8 @@ public class ProductController {
         return "Se están mostrando productos del tipo: " + termino + " ordenado por " + ordenarPor + " de manera " + orden + " y límite " + limite;
     }
 
-    
+    @PostMapping("product")
+    public Product insertProduct(@RequestBody Product producto){
+        return producto;
+    }
 }

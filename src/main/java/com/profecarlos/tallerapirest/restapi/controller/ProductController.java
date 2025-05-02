d6ce4f0d8eec57244b513202d1625c128fe77b00
matiash12 <you@example.com>
2025-05-02 12:58:40 -0400

    tareae

--- src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -1,34 +1,24 @@
 package com.profecarlos.tallerapirest.restapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.profecarlos.tallerapirest.restapi.model.Product;
 
+
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
 
-    @GetMapping("/products")
-    public String getproducst(){
-        return "se listan todos los productos";
-
-
+    @PostMapping("product")
+    public Product insertProduct(@RequestBody Product producto) {
+        
+        
+        return producto;
     }
-    @GetMapping("/product/{id}")
-    public Product  getProduct(@PathVariable long id){
-
-        return new Product(id, "mause",2.000);
-
-    }
-
-    @GetMapping("/api/productos/filtrar")
-    public String filtrarProduct(){
-        return "";
-    }
-
+    
 
   
 
--- src/main/java/com/profecarlos/tallerapirest/restapi/model/Product.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/model/Product.java
@@ -1,37 +1,62 @@
 package com.profecarlos.tallerapirest.restapi.model;
 
 public class Product {
-    private long id;
+    private int id;
     private String nombre;
+    private String categoria;
     private double precio;
+    private String descripcion;
 
-    public Product(long id, String nombre, double precio) {
+    public Product(String categoria, String descripcion, int id, String nombre, double precio) {
+        this.categoria = categoria;
+        this.descripcion = descripcion;
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
     }
-    public long getId() {
+
+    public int getId() {
         return id;
     }
-    
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
     public double getPrecio() {
         return precio;
     }
+
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
     
-    
+
 
     }

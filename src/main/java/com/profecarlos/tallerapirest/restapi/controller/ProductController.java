9ee93d2ac88356d7dc668884b09d98241bf9592e
HachiMakiDev <hachimakidev@gmail.com>
2025-05-02 10:01:51 -0400

    cambios

--- src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.profecarlos.tallerapirest.restapi.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,14 @@ import com.profecarlos.tallerapirest.restapi.model.Product;
 public class ProductController {
 
     @PostMapping("product")
-    public Product insertProduct(){
-        Product producto = new Product(1,"Tarjeta de video", "la mejor", 200.00, "componentes");
+    public Product insertProduct(
+            @RequestBody int id,
+            @RequestBody String nombre,
+            @RequestBody String descripcion,
+            @RequestBody double precio,
+            @RequestBody String categoria) {
+        Product producto = new Product(id, nombre, descripcion, precio, categoria);
         return producto;
     }
-    
+
 }

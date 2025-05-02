3e9c9a443b8db2af5714f16709a4762e0993739d
Gustavo Santana <123342265+Tavotsu@users.noreply.github.com>
2025-05-02 14:10:40 -0400

    Metodos get y post

--- src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/controller/ProductController.java
@@ -1,5 +1,11 @@
 package com.profecarlos.tallerapirest.restapi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.profecarlos.tallerapirest.restapi.model.Product;
 
+
+
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
 
+    private List<Product> productList =new ArrayList<>();
+
     @PostMapping("product")
     public Product insertProduct(@RequestBody Product producto) {
+        
+        int newId = productList.isEmpty() ? 1 : productList.get(productList.size()-1).getId()+1;
+
+        producto.setId(newId);
+        productList.add(producto);
         return producto;
     }
 
+    @GetMapping("products")
+    public List<Product> getAllProducts(){
+        return productList;
+    }
+
+    /*@GetMapping("product/{id}")
+    public ResponseEntity<product> getProductById(@PathVariable int id){
+        return ;
+    }*/
 }

9c6fee2d563be46f76c9cbceb46c76dab53985f5
BeoGonzalez <benjagonzalez@duocuc.cl>
2025-05-14 08:45:44 -0400

    arreglé el ProductController

--- src/main/java/com/profecarlos/tallerapirest/restapi/repository/CompararPreciosRepository.java
+++ src/main/java/com/profecarlos/tallerapirest/restapi/repository/CompararPreciosRepository.java
@@ -1,9 +1,9 @@
-package com.profecarlos.tallerapirest.repository;
-
-import com.profecarlos.tallerapirest.restapi.model.CompararPrecios;
+package com.profecarlos.tallerapirest.restapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.profecarlos.tallerapirest.restapi.model.CompararPrecios;
 
 @Repository
 public interface CompararPreciosRepository extends JpaRepository<CompararPrecios, Integer> {
